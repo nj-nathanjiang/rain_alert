929af05d35bd6267fb3afd8775d8ff245c4f74a9
njiang6 <79889759+njiang6@users.noreply.github.com>
2021-04-04 21:31:49 -0700

    Update rain_alert.py

--- rain_alert.py
+++ rain_alert.py
@@ -2,11 +2,11 @@ import requests
 from twilio.rest import Client
 
 API_ENDPOINT = "https://api.openweathermap.org/data/2.5/onecall"
-MY_LAT = 34.181390
-MY_LON = -118.782680
-api_key = "50ed715298431c237e859993d7e7b032"
-account_sid = "AC7dc2131f6366e61be76f09fb43215cb7"
-auth_token = "c7ead93dcf2371c129a652f4cda3db01"
+MY_LAT = 0 # Latitude
+MY_LON = 0 # Longitude
+api_key = "my_open_weather_data_api_key"
+account_sid = "my_account_sid_for_twilio"
+auth_token = "my_authoration_token_for_twilio"
 
 parameters = {
     "lat": MY_LAT,
@@ -31,7 +31,7 @@ if bring_umbrella:
     client = Client(account_sid, auth_token)
     message = client.messages \
         .create(body="It's going to rain today. Remember to bring an umbrella.",
-                from_='+18305005082',
-                to="+18054051545")
+                from_='+18305005082', # My twilio phone number
+                to="") # My Phone Number
 
     print(message.status)
